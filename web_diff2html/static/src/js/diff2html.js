a3cdeb093053df1430293da360fe5afb5315e890
Andrius Laukavičius <anlaukavic@gmail.com>
2017-05-23 00:07:45 +0300

    [ADD] web_diff2html: config from XML view
    diff2html output can be configured using options as defined in:
    https://github.com/rtfpessoa/diff2html configuration section. Also
    it is possible to pass `extra` config to enable some extra styling
    features (like language syntax highlight)

--- web_diff2html/static/src/js/diff2html.js
+++ web_diff2html/static/src/js/diff2html.js
@@ -5,6 +5,18 @@ var core = require('web.core');
 var editor_backend = require('web_editor.backend')
 var FieldTextHtmlSimple = editor_backend['FieldTextHtmlSimple']
 
+function toJSON(str) {
+    try {
+        if (typeof str != 'string'){
+            return false
+        }
+        // Single quote is often used, so we ignore it.
+        return JSON.parse(str.replace(/'/g, '"'));
+    } catch (e) {
+        return false;
+    }
+}
+
 var FieldDiff2Html = FieldTextHtmlSimple.extend({
     render_value: function() {
         this._super();
@@ -16,20 +28,39 @@ var FieldDiff2Html = FieldTextHtmlSimple.extend({
             // to pass actual value to render it, instead of directly
             // modifying element using ID or class name (as showed in
             // diff2html guide).
-            var diffJson = Diff2Html.getJsonFromDiff(value),
-                cfg = {"inputFormat": 'json', "showFiles": true},
-                diffHtml = Diff2Html.getPrettyHtml(diffJson, cfg);
-                // Convert string HTML to jQuery.
-                var $content = $($.parseHTML(diffHtml));
-                var diff2htmlUi = new Diff2HtmlUI();
-                // Highlight code using jQuery object, because string
-                // type would be used to search for element, not use
-                // actual string HTML.
+            // Get config from field definition in view
+            var cfg = toJSON(this.node.attrs['data-diff2html']) || {
+                "options": {"inputFormat": "diff"}, "extra": {
+                    "highlightCode": true}};
+            if (cfg.options.inputFormat !== 'json'){
+                var diffJson = Diff2Html.getJsonFromDiff(value);
+            } else {
+                // If it is json, we do not need to convert it.
+                diffJson = value;
+            }
+            // Overwrite inputFormat to be jSon, because we will be
+            // using jSon from now on.
+            cfg.options.inputFormat = 'json'
+            var diffHtml = Diff2Html.getPrettyHtml(diffJson, cfg.options),
+            // Convert string HTML to jQuery.
+                $content = $($.parseHTML(diffHtml)),
+                diff2htmlUi = new Diff2HtmlUI();
+            // Highlight code using jQuery object, because string
+            // type would be used to search for element, not use
+            // actual string HTML.
+            if (cfg.extra.highlightCode){
                 diff2htmlUi.highlightCode($content);
-                // Combine lines summary and actual diff lines. And
-                // convert result back to string.
-                this.$content.html(this.text_to_html(
-                    $content[0].outerHTML + $content[1].outerHTML));
+            }
+            if (cfg.extra.fileListCloseable){
+                diff2htmlUi.fileListCloseable($content, false);
+            }
+            // Combine lines summary and actual diff lines. And
+            // convert result back to string.
+            var result = ''
+            _.each($content, function(obj) {
+                result += obj.outerHTML
+            });
+            this.$content.html(this.text_to_html(result))
         }
     }
 })
