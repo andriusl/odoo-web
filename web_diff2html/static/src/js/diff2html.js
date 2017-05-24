aa6af4801fd53dfc3ecc6211789e68bbb13754d5
Andrius Laukavičius <anlaukavic@gmail.com>
2017-05-24 21:41:39 +0300

    [REF] web_diff2html: docstrings
    Explaining working and usage of functions.

--- web_diff2html/static/src/js/diff2html.js
+++ web_diff2html/static/src/js/diff2html.js
@@ -5,6 +5,12 @@ var core = require('web.core');
 var editor_backend = require('web_editor.backend')
 var FieldTextHtmlSimple = editor_backend['FieldTextHtmlSimple']
 
+/**
+ * Convert string hashmap into jSon object.
+ * @param  {string} str string that represent jSon object.
+ * @return {jSon or false} if valid Json representation is provided, it
+ * returns jSon object, otherwise returns false.
+ */
 function toJSON(str) {
     try {
         if (typeof str != 'string'){
@@ -18,6 +24,12 @@ function toJSON(str) {
 }
 
 var FieldDiff2Html = FieldTextHtmlSimple.extend({
+    /**
+     * Build options and extra options config to apply different effects
+     * on diff2html styling.
+     * @return {object} configuration object. If no data was specified
+     *     in XML, return object with default values.
+     */
     get_config: function() {
         var cfg = toJSON(this.node.attrs['data-diff2html']) || {};
         // Add default objects for options and/or extra if they are were
@@ -29,6 +41,17 @@ var FieldDiff2Html = FieldTextHtmlSimple.extend({
         return cfg;
 
     },
+    /**
+     * Apply extra options like code highlighting or closeable files
+     * list. Can be extended to apply additional options.
+     * @param  {object} extra  config containing extra options.
+     * @param  {string or jQuery} target target that options should be
+     *     applied. If string is specified, it expects to be identifier
+     *     for element like ID or classname. Note using string, might
+     *     not work for this widget out of the box.
+     * @return {diff2htmlUi} instance extra options are applied on. Can
+     *     be used easier extension to apply additional options.
+     */
     apply_extra: function(extra, target) {
         var diff2htmlUi = new Diff2HtmlUI();
         if (extra.highlightCode){
@@ -43,6 +66,10 @@ var FieldDiff2Html = FieldTextHtmlSimple.extend({
         // Return this object for easier extendability.
         return diff2htmlUi;
     },
+    /**
+     * Render diff into HTML. diff2html is only active in readonly mode.
+     * @return {undefined}
+     */
     render_value: function() {
         this._super();
         // We access value using jQuery, because using Odoo
