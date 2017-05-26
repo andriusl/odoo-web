51384d2d5b31d57fbd2db0b8f212e61b34b784e7
Andrius Laukavičius <anlaukavic@gmail.com>
2017-05-26 20:51:13 +0300

    [IMP] web_diff2html: index.html width
    It is harder to read when content of index.html is stretched out through
    whole screen width, so we use Odoo standard CSS classes to manage width.

--- web_diff2html/__manifest__.py
+++ web_diff2html/__manifest__.py
@@ -3,7 +3,7 @@
 # See LICENSE file for full copyright and licensing details.
 {
     'name': "Diff To HTML",
-    'version': '1.0.1',
+    'version': '1.1.0',
     'category': 'Web',
     'summary': 'web, widget, diff, html',
     'author': "Andrius Laukavičius",
